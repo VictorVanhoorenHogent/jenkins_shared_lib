82bd98800fb24d6552742ad4fc7b652f09624175
Victor Vanhooren <victor.vanhooren@student.hogent.be>
2023-04-08 16:05:19 +0200

    toevoegen push naar ecr

--- vars/dockerImagePushECR.groovy
+++ vars/dockerImagePushECR.groovy
@@ -9,9 +9,9 @@ def call(String AwsRegion, String AwsAccountId, String EcrRepo){
 
 
 
-   docker.withRegistry(
-        "https://${AwsAccountId}.dkr.ecr.${AwsRegion}.amazonaws.com",
-        "ecr:${AwsRegion}:AwsCredentials"
-    ) {
-        docker.image("${AwsAccountId}.dkr.ecr.${AwsRegion}.amazonaws.com/${EcrRepo}:latest").push()
-    } 
+//   docker.withRegistry(
+//        "https://${AwsAccountId}.dkr.ecr.${AwsRegion}.amazonaws.com",
+//        "ecr:${AwsRegion}:AwsCredentials"
+//    ) {
+//        docker.image("${AwsAccountId}.dkr.ecr.${AwsRegion}.amazonaws.com/${EcrRepo}:latest").push()
+//    } 
