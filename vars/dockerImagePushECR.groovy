b4083794460f69d0c3a7377a9ab9f8e561e352c9
Victor Vanhooren <victor.vanhooren@student.hogent.be>
2023-04-08 16:06:22 +0200

    toevoegen push naar ecr

--- vars/dockerImagePushECR.groovy
+++ vars/dockerImagePushECR.groovy
@@ -1,7 +1,7 @@
 def call(String AwsRegion, String AwsAccountId, String EcrRepo){
     sh"""
     aws ecr get-login-password --region ${AwsRegion} | docker login --username AWS --password-stdin ${AwsAccountId}.dkr.ecr.${AwsRegion}.amazonaws.com
-    docker push ${EcrRepo}:latest ${AwsAccountId}.dkr.ecr.${AwsRegion}.amazonaws.com/${EcrRepo}:latest
+    docker push ${AwsAccountId}.dkr.ecr.${AwsRegion}.amazonaws.com/${EcrRepo}:latest
     """
 }
 
