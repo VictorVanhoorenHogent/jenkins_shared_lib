50a50535dc3e12aea88b6362f2078055b5c7b3aa
Victor Vanhooren <victor.vanhooren@student.hogent.be>
2023-04-08 15:29:20 +0200

    toevoegen push naar ecr

--- vars/dockerImagePushECR.groovy
+++ vars/dockerImagePushECR.groovy
@@ -1,17 +1,20 @@
 def call(String AwsRegion, String AwsAccountId, String EcrRepo){
-    sh"""
-    aws ecr get-login-password --region ${AwsRegion} | docker login --username AWS --password-stdin ${AwsAccountId}.dkr.ecr.${AwsRegion}.amazonaws.com
-    docker push ${EcrRepo}:latest ${AwsAccountId}.dkr.ecr.${AwsRegion}.amazonaws.com/${EcrRepo}:latest
-    """
+   
+   docker.withRegistry(
+        "https://${AwsAccountId}.dkr.ecr.${AwsRegion}.amazonaws.com",
+        "ecr:${AwsRegion}:AwsCredentials"
+    ) {
+        docker.image("${AwsAccountId}.dkr.ecr.${AwsRegion}.amazonaws.com/${EcrRepo}:latest").push()
+    } 
 }
 
 
 
 
 
-//   docker.withRegistry(
-//        "https://${AwsAccountId}.dkr.ecr.${AwsRegion}.amazonaws.com",
-//        "ecr:${AwsRegion}:AwsCredentials"
-//    ) {
-//        docker.image("${AwsAccountId}.dkr.ecr.${AwsRegion}.amazonaws.com/${EcrRepo}:latest").push()
-//    } 
+   docker.withRegistry(
+        "https://${AwsAccountId}.dkr.ecr.${AwsRegion}.amazonaws.com",
+        "ecr:${AwsRegion}:AwsCredentials"
+    ) {
+        docker.image("${AwsAccountId}.dkr.ecr.${AwsRegion}.amazonaws.com/${EcrRepo}:latest").push()
+    } 
