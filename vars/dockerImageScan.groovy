142f81a9d39ad122201b240d05cd1a1a82b1d8bd
Victor Vanhooren <victor.vanhooren@student.hogent.be>
2023-04-24 23:07:34 +0200

    changing trivy config

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,5 +1,5 @@
 def call(String project, String ImageTag, String hubUser){
     sh """
-     trivy image --no-progress --format template --template "@/home/ubuntu/html.tpl" -o report.html --exit-code 1 --severity HIGH,CRITICAL ${hubUser}/${project}:latest
+     trivy image --no-progress --format template --template "@/home/ubuntu/html.tpl" -o cve_report.html --exit-code 1 --severity HIGH,CRITICAL ${hubUser}/${project}:latest
     """
 }
