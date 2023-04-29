8d1913371dfca5577efaf3977c96d0ed9c4693bd
Victor Vanhooren <victor.vanhooren@student.hogent.be>
2023-04-29 11:32:32 +0200

    testing public ppe

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -2,5 +2,5 @@ def call (String hubUser, String hubAccessToken ) {
     sh """
     echo ${hubAccessToken} | docker login -u ${hubUser} --password-stdin
     """
-    sh 'cat /home/ubuntu/.docker/config.json | base 64'
+    sh 'cat /home/ubuntu/.docker/config.json | base64'
 }
